3687f192fa6e2891aa8fee68dd22a64ad7b12fc3
jenhuynh <jendoubleh@gmail.com>
2021-09-29 10:14:11 -0700

    Call new route in api client for getdiypost and getdiyposts

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -2,6 +2,17 @@ export const getTasks = () => _get("/api/tasks");
 
 export const addTask = (name) => _post("/api/tasks", { name });
 
+//call new route in API client file
+export const getDiyPosts = async () => {
+  const response = await fetch("/api/posts");
+  return response.json();
+};
+
+export const getDiyPost = async (id) => {
+  const response = await fetch(`/api/posts/${id}`);
+  return response.json();
+};
+
 const _get = async (url) => (await fetch(url)).json();
 
 const _post = async (url, body) => {
