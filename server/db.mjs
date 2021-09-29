1e10dc1e6b8af9d256f396de672adc9a7c8e4570
jenhuynh <jendoubleh@gmail.com>
2021-09-29 10:17:47 -0700

    Add sql queries for getdiypost and gitdiyposts

--- server/db.mjs
+++ server/db.mjs
@@ -8,6 +8,13 @@ export const getTasks = () => db.any("SELECT * FROM tasks");
 export const addTask = (name) =>
   db.one("INSERT INTO tasks(name) VALUES($<name>) RETURNING *", { name });
 
+//gets all data from db
+export const getDiyPosts = () => db.any("SELECT * FROM diypost");
+
+//gets a single post from db, for every intropolation (every pair of braces aka {postId}), each have keys, each intropolation substitute the value and goes to find value of id in db and put it in {postId}
+export const getDiyPost = (postId) =>
+  db.one("SELECT * FROM diypost WHERE id = ${postId}", { postId });
+
 function initDb() {
   let connection;
 
